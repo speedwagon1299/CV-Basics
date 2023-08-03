15860cb2f4c9f4906985592d4051a045a93679f3
speedwagon1299 <srihari5202004@gmail.com>
2023-08-03 20:38:34 +0530

    first commit (real)

--- read.py
+++ read.py
@@ -1,4 +1,19 @@
 import cv2 as cv
+
+# reading an image
+
 img = cv.imread("opencv-course-master/Resources/Photos/cat.jpg")
 cv.imshow('cat image',img)
-cv.waitKey(0)
+cv.waitKey(0) #0 is for any key on the keyboard
+
+# reading a video
+vid = cv.VideoCapture('opencv-course-master/Resources/Videos/dog.mp4')
+
+while True:     #while frame is availabe show the video
+    isTrue, frame = vid.read()  #read method to take frame by frame
+    cv.imshow('dog video', frame)
+    if cv.waitKey(20) & 0xff == ord('d'):   #after 20 ns if the user hits 'd' then exit
+        break
+
+vid.release()
+cv.destroyAllWindows()
---
+++ rescale.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+
+#rescaling frames
+def rescaleFrame(frame, scale = 0.75):
+    width = int(frame.shape[1]*scale)   #width of matrix is num of columns
+    height = int(frame.shape[0]*scale)  #float converted to int
+    dim = (width,height)    #dimension for some reason stored in opposite order
+    return cv.resize(frame, dim, interpolation=cv.INTER_AREA)
+
+# changing vid resolution ONLY FOR LIVE VIDEO
+# def changeRes(width,height):
+#    cap.set(3,width)
+#    cap.set(4,height)
+
+cap = cv.VideoCapture('/home/speedwagon1299/projects/CV-Basics/opencv-course-master/Resources/Videos/dog.mp4')
+
+while True:
+    isTrue, frame = cap.read()
+    cv.imshow('dog vid normal', frame)
+    frame_new = rescaleFrame(frame)
+    cv.imshow('dog vid scale', frame_new)
+    if cv.waitKey(20) & 0xff == ord('d'):       #20ms per frame is golden number
+        break
+cap.release()
+cv.destroyAllWindows()
